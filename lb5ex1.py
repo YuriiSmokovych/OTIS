5858773fd9a3958e1ce2c38d6fd200a8bf66a913
Yurii Smokovych <113466626+YuriiSmokovych@users.noreply.github.com>
2022-09-20 11:28:00 +0300

    Update lb5ex1.py

--- lb5ex1.py
+++ lb5ex1.py
@@ -1,10 +1,11 @@
 import math
-x = float(input('Введіть x: '))
+x1 = float(input('Введіть x: '))
 def func (x):
     if x > 12.1:
        return math.log(abs(x-1), math.e)+ math.log10(abs(x**0.7*x+2))
-    if x >=-5.7 and x <= 12.1:
+    elif x >=-5.7 and x <= 12.1:
         return math.e*4*x**7+2 - x**1/3
     else:
         return 4.27*x+4.33*x**2+math.sin(x+1)
-print (func(x))
+f=(func(x1))
+print (f)
