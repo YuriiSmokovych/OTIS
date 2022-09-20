d05714ab90a88a2e844aba4b22847fccd5ce916d
Yurii Smokovych <113466626+YuriiSmokovych@users.noreply.github.com>
2022-09-20 11:28:35 +0300

    Update lb4ex2.py

--- lb4ex2.py
+++ lb4ex2.py
@@ -1,8 +1,9 @@
 import math
-x=float(input("Введіть х"))
-y=float(input("Введіть y")) 
-z=float(input("Введіть z")) 
-def func1 (x):
+x1=float(input("Введіть х"))
+y1=float(input("Введіть y")) 
+z1=float(input("Введіть z")) 
+def func1 (x, y, z):
     a = math.pow(0.7*math.cos(x) + math.log(abs(y)), 1/5) + math.pow(math.e, z+x)
     return (a)
-print (func1(x))
+f= func1(x1,y1,z1)
+print (f)
